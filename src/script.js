04a615b913e0ccb9e6b23a45325247087ec7d1d1
Ângelo Baracho <92029180+Anbfer@users.noreply.github.com>
2023-02-10 09:06:55 -0300

    Delete src directory

--- src/script.js
+++
@@ -1,9 +0,0 @@
-function mudaTemas() {
-	document.body.classList.toggle("light");
-	var botao = document.querySelector("#botaoTema");
-	if (botao.textContent == "Modo Dark") {
-		botao.textContent = "Modo Light";
-	} else {
-		botao.textContent = "Modo Dark";
-	}
-}
